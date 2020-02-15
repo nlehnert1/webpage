9399ea289362e642cfb9d6e25a822f56f65015b9
Chloe Lehnert <nlehnert2@unl.edu>
2020-02-15 14:32:25 -0600

    initial commit

---
+++ steamlink.js
@@ -0,0 +1,6 @@
+function getSteamStats() {
+    const apiKey = "EC633BE02C242B2A0926B78F2D5C945D";
+    const api = "https://api.steampowered.com/";
+
+    alert("You found out what the submit button does!");
+}
